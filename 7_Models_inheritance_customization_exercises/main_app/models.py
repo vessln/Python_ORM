43f84d04d9bf38bde53d33951fc36d9cfe55c1ab
Veselina S <vesi.as@abv.bg>
2023-11-07 16:04:01 +0200

    add exercises

---
+++ 7_Models_inheritance_customization_exercises/main_app/models.py
@@ -0,0 +1,291 @@
+from datetime import timedelta
+
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class BaseCharacter(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Mage(BaseCharacter):
+    elemental_power = models.CharField(
+        max_length=100,
+    )
+    spellbook_type = models.CharField(
+        max_length=100,
+    )
+
+
+class Assassin(BaseCharacter):
+    weapon_type = models.CharField(
+        max_length=100,
+    )
+    assassination_technique = models.CharField(
+        max_length=100,
+    )
+
+
+class DemonHunter(BaseCharacter):
+    weapon_type = models.CharField(
+        max_length=100,
+    )
+    demon_slaying_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class TimeMage(Mage):
+    time_magic_mastery = models.CharField(
+        max_length=100,
+    )
+    temporal_shift_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class Necromancer(Mage):
+    raise_dead_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class ViperAssassin(Assassin):
+    venomous_strikes_mastery = models.CharField(
+        max_length=100,
+    )
+    venomous_bite_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class ShadowbladeAssassin(Assassin):
+    shadowstep_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class VengeanceDemonHunter(DemonHunter):
+    vengeance_mastery = models.CharField(
+        max_length=100,
+    )
+    retribution_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class FelbladeDemonHunter(DemonHunter):
+    felblade_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class UserProfile(models.Model):
+    username = models.CharField(
+        max_length=70,
+        unique=True,
+    )
+    email = models.EmailField(
+        unique=True,
+    )
+    bio = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        to="UserProfile",
+        related_name="sent_messages",
+        on_delete=models.CASCADE,
+    )
+    receiver = models.ForeignKey(
+        to="UserProfile",
+        related_name="received_messages",
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField()
+
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+    )
+    is_read = models.BooleanField(
+        default=False,
+    )
+
+    def mark_as_read(self):
+        self.is_read = True
+        self.save()
+
+    def mark_as_unread(self):
+        self.is_read = False
+        self.save()
+
+    def reply_to_message(self, reply_content, receiver):
+        reply_message = Message(sender=self.receiver, receiver=receiver, content=reply_content)
+        reply_message.save()
+        return reply_message
+
+    def forward_message(self, sender, receiver):
+        forward_message = Message(sender=sender, receiver=receiver, content=self.content)
+        forward_message.save()
+        return forward_message
+
+
+class StudentIDField(models.PositiveIntegerField):
+
+    def from_db_value(self, value, expression, connection):
+        if value is None:
+            return value
+
+        return int(value)
+
+# #    second option:
+#     def to_python(self, value):
+#         try:
+#             return int(value)
+#         except ValueError:
+#             pass  # raise ValidationError("Wrong type")
+
+class Student(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    student_id = StudentIDField()
+
+
+class MaskedCreditCardField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_length"] = 20
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not isinstance(value, str):
+            raise ValidationError("The card number must be a string")
+
+        if not value.isdigit():
+            raise ValidationError("The card number must contain only digits")
+
+        if len(value) != 16:
+            raise ValidationError("The card number must be exactly 16 characters long")
+
+        return f"****-****-****-{value[12:]}"
+
+
+class CreditCard(models.Model):
+    card_owner = models.CharField(
+        max_length=100,
+    )
+    card_number = MaskedCreditCardField()
+
+
+class Hotel(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    address = models.CharField(
+        max_length=200,
+    )
+
+
+class Room(models.Model):
+    hotel = models.ForeignKey(
+        to="Hotel",
+        on_delete=models.CASCADE,
+    )
+    number = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    capacity = models.PositiveIntegerField()
+
+    total_guests = models.PositiveIntegerField()
+
+    price_per_night = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    def clean(self):
+        if self.capacity < self.total_guests:
+            raise ValidationError("Total guests are more than the capacity of the room")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+        return f"Room {self.number} created successfully"
+
+
+class BaseReservation(models.Model):
+    room = models.ForeignKey(
+        to="Room",
+        on_delete=models.CASCADE,
+    )
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    class Meta:
+        abstract = True
+
+    def reservation_period(self):
+        return (self.end_date - self.start_date).days
+
+    def calculate_total_cost(self):
+        cost = self.room.price_per_night * self.reservation_period()
+        total_cost = round(cost, 1)
+        return total_cost
+
+    @property
+    def check_res_is_available(self):
+        current_reservations = self.__class__.objects.filter(
+            room=self.room,
+            end_date__gte=self.start_date,
+            start_date__lte=self.end_date,
+        )
+        return True if not current_reservations.exists() else False
+
+    def clean(self):
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date cannot be after or in the same end date")
+
+        if not self.check_res_is_available:
+            raise ValidationError(f"Room {self.room.number} cannot be reserved")
+
+
+class RegularReservation(BaseReservation):
+    def save(self, *args, **kwargs):
+        super().clean()
+        super().save(*args, **kwargs)
+
+        return f"Regular reservation for room {self.room.number}"
+
+
+class SpecialReservation(BaseReservation):
+    def save(self, *args, **kwargs):
+        super().clean()
+        super().save(*args, **kwargs)
+
+        return f"Special reservation for room {self.room.number}"
+
+    def extend_reservation(self, days):
+        current_reservations = SpecialReservation.objects.filter(
+            room=self.room,
+            end_date__gte=self.start_date,
+            start_date__lte=self.end_date + timedelta(days=days),
+        )
+        if current_reservations:
+            raise ValidationError("Error during extending reservation")
+
+        self.end_date += timedelta(days=days)
+        self.save()
+
+        return f"Extended reservation for room {self.room.number} with {days} days"
+
